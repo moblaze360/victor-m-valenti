4d3b1a3731c7d11fb366fd4e17cc4b6941fdbb0d
moblaze360 <victorvalenti@yahoo.co.uk>
2021-01-08 15:54:28 +0200

    Updates

---
+++ precache-manifest.8e62cbb4196dddfead59c263ea6c9736.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7398d6a450dad2584e62bc002000410c",
+    "url": "/myreact-app/index.html"
+  },
+  {
+    "revision": "b446806bf7f3509a77ae",
+    "url": "/myreact-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0db2159c17df05b54294",
+    "url": "/myreact-app/static/css/main.ff72a416.chunk.css"
+  },
+  {
+    "revision": "b446806bf7f3509a77ae",
+    "url": "/myreact-app/static/js/2.cea91052.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact-app/static/js/2.cea91052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db2159c17df05b54294",
+    "url": "/myreact-app/static/js/main.92f07c6e.chunk.js"
+  },
+  {
+    "revision": "0a1ef42ef36a615a1e15",
+    "url": "/myreact-app/static/js/runtime-main.27bf8fb4.js"
+  },
+  {
+    "revision": "fdf6b5f411d638bb6ffd4ee072e25716",
+    "url": "/myreact-app/static/media/Architex.fdf6b5f4.woff"
+  },
+  {
+    "revision": "5b3ff6b6f01f17a9e58278f51ba60148",
+    "url": "/myreact-app/static/media/LinlegreyRegular-Oo3O.5b3ff6b6.woff"
+  },
+  {
+    "revision": "195d0634b97cc5727e78d2f81f80067d",
+    "url": "/myreact-app/static/media/Money-mu.195d0634.svg"
+  },
+  {
+    "revision": "814db3fcbb4a092cf9ea63fabe00f8ce",
+    "url": "/myreact-app/static/media/NewSuperman-Pzd7.814db3fc.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myreact-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e8c6d7183fba1d931fecffa4953f3e69",
+    "url": "/myreact-app/static/media/YellowtailRegular.e8c6d718.woff"
+  },
+  {
+    "revision": "9eea60810b6d0370a1e1b2a16402201d",
+    "url": "/myreact-app/static/media/apache.9eea6081.svg"
+  },
+  {
+    "revision": "0172c651ecde833ba460713268daeeae",
+    "url": "/myreact-app/static/media/batmfa__.0172c651.woff"
+  },
+  {
+    "revision": "e6e10fdf66cf9f4f84d2b269586cf040",
+    "url": "/myreact-app/static/media/batmfo__.e6e10fdf.woff"
+  },
+  {
+    "revision": "03a46c9c99e33fa0808d490d6ba83533",
+    "url": "/myreact-app/static/media/bootstrap.03a46c9c.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/myreact-app/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "f4a15b1d8a3dedd671c0205f52d24609",
+    "url": "/myreact-app/static/media/drupal.f4a15b1d.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/myreact-app/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/myreact-app/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "1802a17c498c47c5811f7c63586b5415",
+    "url": "/myreact-app/static/media/joomla.1802a17c.svg"
+  },
+  {
+    "revision": "2642ea54dd4c6222457c92c6437a1049",
+    "url": "/myreact-app/static/media/jquery.2642ea54.svg"
+  },
+  {
+    "revision": "97f746a5100564bee8906bfaaee3aa99",
+    "url": "/myreact-app/static/media/linux.97f746a5.svg"
+  },
+  {
+    "revision": "f344158d2c27f41d197522f243c5f758",
+    "url": "/myreact-app/static/media/mongodb.f344158d.svg"
+  },
+  {
+    "revision": "309039a394f8de87b51feb4d9cd0ae86",
+    "url": "/myreact-app/static/media/mysql.309039a3.svg"
+  },
+  {
+    "revision": "e1d7fcc979c1d40ac116c43a21cb97df",
+    "url": "/myreact-app/static/media/node-dot-js.e1d7fcc9.svg"
+  },
+  {
+    "revision": "a9f29af417ebe3a13c7176e78553e2aa",
+    "url": "/myreact-app/static/media/php.a9f29af4.svg"
+  },
+  {
+    "revision": "a9894ba60d9a250a69dd9f1c66080010",
+    "url": "/myreact-app/static/media/postgresql.a9894ba6.svg"
+  },
+  {
+    "revision": "2f6f7ef6b98ddf93ed8ffa09cfce7103",
+    "url": "/myreact-app/static/media/react.2f6f7ef6.svg"
+  },
+  {
+    "revision": "afb8dfcddb4f49c3cf5a4d09b4e73d4a",
+    "url": "/myreact-app/static/media/wingitBeztech-transparent.afb8dfcd.png"
+  },
+  {
+    "revision": "1f6ae9caf5eeacf982c9ef96101676ed",
+    "url": "/myreact-app/static/media/wingitLMS-transparent.1f6ae9ca.png"
+  },
+  {
+    "revision": "b96c87c0f27557313be48fc6d5aff107",
+    "url": "/myreact-app/static/media/wingitfaceDec - transparent_1.b96c87c0.png"
+  },
+  {
+    "revision": "e9fd520fc1e8316bce96dd78a83900e0",
+    "url": "/myreact-app/static/media/wingitfaceDec-moneyMu.e9fd520f.png"
+  },
+  {
+    "revision": "16d08ccbd919ef1281ae09664243cccf",
+    "url": "/myreact-app/static/media/wingitfaceDec-tecfinder.16d08ccb.png"
+  },
+  {
+    "revision": "54dbeb12d2610f9ba037a0635f196660",
+    "url": "/myreact-app/static/media/wingittasks-transparent.54dbeb12.png"
+  },
+  {
+    "revision": "d960ce447fab10a358b4d3ba62daa1a4",
+    "url": "/myreact-app/static/media/wordpress.d960ce44.svg"
+  }
+]);
